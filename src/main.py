f57918a836f0ed10fe24a95dffa674a4335453a7
NurHary <theliq310@gmail.com>
2025-12-16 15:41:38 +0700

    ok receive

--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
-# Untuk Parsing CLI
-import argparse
+"""
+
+Scanner App: Aplikasi scanner yang dikhususkan untuk orang - orang dengan keterbatasan pada device mobile dalam menggunakan CamScanner
+
+"""
+
+# // Import cv2 / opencv for image processing
 import cv2 as opcv
+
+# // Import flask for http server
 import flask
+
+# // Import os for file management
+import os
+
+# // Import For Filepath
+import pathlib
+
+
+# Global Constant Raw Path, path for Raw Image
+GC_FILE_PATH = pathlib.Path(__file__).parent.resolve()
 
 
 app = flask.Flask(__name__)
@@ -19,17 +36,43 @@ def ping():
 
 @app.route("/pong", methods=["POST"])
 def upload():
-    # TODO:
+    # Header
+
+    saved_filename = ""
+    if "image" not in flask.request.files:
+        print("one 1")
+        return "one 1"
 
     # Lakukan Get Data,
+    file = flask.request.files["image"]
 
-    # Simpan pada src/img/
+    # Match Guard
+    match file.filename:
+        # // To Counter if None
+        case None:
+            print("one 2")
+            return "one 2"
 
-    # Fungsi SCAN
+        # // For Any
+        case _:
+            # for saving filepath
+            saved_filename = str("img/raw/Raw_") + file.filename
 
-    # Fungsi mengirimkan hasil ke clipboard
+            # Simpan pada src/img/
+            # Save This For Later Disposal
+            filepath = os.path.join(GC_FILE_PATH, saved_filename)
 
-    return "Server hidup"
+            # DEBUG
+            print("Filess=====: " + filepath)
+            file.save(filepath)
+
+            # Fungsi SCAN
+
+            # Fungsi mengirimkan hasil ke clipboard
+            # // todo
+
+            print("All Is Done")
+            return "All Is Done"
 
 
-app.run(host="0.0.0.0", port=8000)
+app.run(host="0.0.0.0", port=5000)
